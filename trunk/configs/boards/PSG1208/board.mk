db8e7e22db40798b1bb05da6c10999939fdb9eb9
yuntao1223 <35868506+yuntao1223@users.noreply.github.com>
2022-06-03 14:02:54 +0800

    Update board.mk

--- trunk/configs/boards/PSG1208/board.mk
+++ trunk/configs/boards/PSG1208/board.mk
@@ -4,5 +4,5 @@
 # PSG1208   # PHICOMM PSG1208  # MT7620  #
 ##################################################################
 
-CFLAGS += -DBOARD_PSG1208
+CFLAGS += -DBOARD_LG522
 BOARD_NUM_USB_PORTS=0
